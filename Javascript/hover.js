296374e3c354e4fc60bc6836141e03aa1061a659
Dave <103978471+Grm1n@users.noreply.github.com>
2023-05-17 13:00:13 -0400

    Delete Javascript directory

--- Javascript/hover.js
+++
@@ -1,6 +0,0 @@
-$(".profileselect").on("mouseover", function () {
-    $(this).parent().parent().addClass("hovered")
-  })
-  $(".profileselect").on("mouseout", function () {
-    $(this).parent().parent().removeClass("hovered")
-  })
